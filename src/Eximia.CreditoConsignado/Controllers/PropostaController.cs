91f60b1dc99513f1dfa0081213c706fbd0d35fbf
gustavo.cardoso <gustavo.pc.ccm@gmail.com>
2025-03-03 11:22:02 -0300

    Criação e estrutura inicial do projeto

---
+++ src/Eximia.CreditoConsignado/Controllers/PropostaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eximia.CreditoConsignado.Controllers
+{
+    public class PropostaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
